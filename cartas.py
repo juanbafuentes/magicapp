ace7d8a7dfd94990d18969acf56c02c62642810b
Juanba Fuentes <juanbafuentes@gmail.com>
2024-03-25 21:04:08 +0100

    Instalamos Reflex

--- cartas.py
+++ cartas.py
@@ -1,10 +1,34 @@
 import requests
+import json
 
-def carta(id):
+def carta(id) -> str:
 
-    url = f'https://api.magicthegathering.io/v1/cards/?number=333'
-    
-    requests.post(url)
-    response = requests.post(url)
+    url = "https://api.magicthegathering.io/v1/cards?number={id}"
+    response = requests.get(url)
 
-    return response
+    if response.status_code == 200:
+        # La solicitud fue exitosa
+        datos = response.json()
+        with open("resultado.json", "w") as archivo:
+            json.dump(datos, archivo, indent=4)
+    else:
+        # La solicitud falló
+        print("Error:", response.status_code)
+
+    # # Obtiene el nombre del primer resultado.
+    name = datos["cards"][0]["foreignNames"][1]["name"]
+    manacost = datos["cards"][0]["manaCost"]
+    type = datos["cards"][0]["foreignNames"][1]["type"]
+    rarity = datos["cards"][0]["rarity"]
+    foreignNames = datos["cards"][0]["foreignNames"][1]["text"]
+    imageURL = datos["cards"][0]["foreignNames"][1]["imageUrl"]
+
+
+    print(f"""Nombre: {name}
+        Coste: {manacost}
+        Tipo: {type}
+        Rareza: {rarity}
+        Descripción: {foreignNames}
+        Imagen: {imageURL}
+    """)
+
